8c27d514a9b8bf17df77d239f69d5b31022f1636
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-22 16:12:10 +0100

    feature: Implementing the Paragraph parse support
    The paragraph support enables to parse raw data into <p>..</p> like format

---
+++ src/MdLight/Markdown/Blocs/Paragraph.cs
@@ -0,0 +1,29 @@
+using MdLight.Markdown.Base;
+
+namespace MdLight.Markdown.Blocs;
+
+internal class Paragraph : Bloc
+{
+    public static readonly Paragraph Default = new(string.Empty);
+    protected override string RawContent { get; set; }
+    protected override IList<Bloc>? Content { get; set; }
+    private Paragraph(string content)
+    {
+        RawContent = content;
+    }
+
+    public override (Bloc, int) Create(IEnumerable<string> lines, int startIndex)
+    {
+        return (new Paragraph(lines.ElementAt(startIndex)), startIndex++);
+    }
+
+    public override bool Match(IEnumerable<string> lines, int startIndex)
+    {
+        return false;
+    }
+
+    public override string ToHtml()
+    {
+        return $"<p>{RawContent}</p>";
+    }
+}
