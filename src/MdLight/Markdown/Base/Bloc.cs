b7c0e916f5b0282fb847d17ff89d9f5ff0360fd2
Djoufson CHE BENE <djouflegran@gmail.com>
2023-10-22 14:40:39 +0100

    code: Bloc base class updated.
    The Matching process is now delegated to each bloc

--- src/MdLight/Markdown/Base/Bloc.cs
+++ src/MdLight/Markdown/Base/Bloc.cs
@@ -2,9 +2,9 @@ namespace MdLight.Markdown.Base;
 
 internal abstract class Bloc
 {
-    public abstract string Pattern { get; }
     protected abstract string RawContent { get; set; }
     protected abstract IList<Bloc>? Content { get; set; }
-
     public abstract string ToHtml();
+    public abstract Bloc Create(IEnumerable<string> lines, int startIndex);
+    public abstract bool Match(IEnumerable<string> lines, int startIndex);
 }
